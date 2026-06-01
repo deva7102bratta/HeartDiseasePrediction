ac0df990c200f20412b6f241d9cec35c10014c3f
deva7102bratta <devabrattayumnam@gmail.com>
2026-06-01 08:04:27 +0530

    updated files

--- src/predict.py
+++ src/predict.py
@@ -1,29 +1,11 @@
 from tensorflow.keras.models import load_model
 import numpy as np
-import pandas as pd
 
 model = load_model("./models/heart_disease_model_v3.keras")
 
-# means = pd.read_csv("../data/means.csv")
-# stds = pd.read_csv("../data/stds.csv")
-
-# age_mean = 54.97
-# trestbps_mean = 132.35
-# chol_mean = 247.45
-# thalach_mean = 147.62
-# oldpeak_mean = 1.10
-# slope_mean = 1.60
-
-# age,8.885122458722824
-# trestbps,18.186912785860557
-# chol,49.54693487176298
-# thalach,23.463325904851786
-# oldpeak,1.183855640287323
-# slope,0.6309776383383059
-
 means = [54.97, 132.35, 247.45, 147.62, 1.10, 1.60]
-stds = [8.885122458722824, 18.186912785860557, 49.54693487176298, 23.463325904851786, 1.183855640287323, 0.6309776383383059
-]
+stds  = [8.885122458722824, 18.186912785860557, 49.54693487176298,
+         23.463325904851786, 1.183855640287323, 0.6309776383383059]
 
 feature_names = [
     "age","trestbps","chol","thalach","oldpeak","slope",
@@ -38,27 +20,29 @@ feature_names = [
 
 input_data = []
 
+# numeric features (normalized)
 for i in range(6):
     while True:
         try:
             value = float(input(f"{feature_names[i]}: "))
-            input_data.append((value-means[i])/stds[i])
+            input_data.append((value - means[i]) / stds[i])
             break
-        except:
+        except ValueError:
             print("Invalid input!")
-for i in range(6:):
+
+# categorical / one-hot features (raw)
+for i in range(6, len(feature_names)):
     while True:
         try:
-            value = float(input(f"{feature_names[i]}: "))
+            value = float(input(f"{feature_names[i]} (0 or 1): "))
             input_data.append(value)
             break
-        except:
-            print("Invalid input!")  
-            
-input_data = np.array(input_data).reshape(1, -1)
-print(input_data)
-print(input_data.shape)
-# Predict
-# prediction = model.predict(input_data)
+        except ValueError:
+            print("Invalid input!")
 
-# print(prediction)
+input_data = np.array(input_data).reshape(1, -1)
+
+print("Input shape:", input_data.shape)
+
+prediction = model.predict(input_data)
+print("Prediction:", prediction)
